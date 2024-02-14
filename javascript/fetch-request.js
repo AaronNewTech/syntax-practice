7e6759d3705fe11a853ba806421039ee56c74dc2
Aaron Smith <arnhsmith@gmail.com>
2024-02-13 20:19:00 -0500

    added fetch-request.js

--- javascript/fetch-request.js
+++ javascript/fetch-request.js
@@ -1,18 +1,29 @@
+// async function that waits for the user to complete the request
 const fetchLaunchData = async () => {
-    try {
-        const response = await fetch('https://api.spacexdata.com/v4/launches')
-        if (response.ok) {
-            const data = await response.json()
-            console.log(data)
-        }
-        else {
-            console.error('Failed to fetch launch data: ' + response.status)
-        } 
-    }
-        catch (error) {
-            console.error('Failed to fetch launch data: ', error)
-        }
-    
-}
 
-fetchLaunchData()
+    // try block to execute request
+    try {
+
+        // defining the request and capturing it in the response variable
+        const response = await fetch('https://api.spacexdata.com/v4/launches');
+
+        // check if response is successful and if not throw error with error message and response status
+        if (!response.ok) {
+            throw new Error('Failed to fetch launch data: ' + response.status);
+        }
+        
+        // if response is successful parse the response body as JSON
+        const data = await response.json();
+
+        // print the launch data to the console
+        console.log(data);
+    }
+    
+    // log error console log error message if try block failed
+    catch (error) {
+        console.error('Failed to fetch launch data:', error);
+    }
+};
+
+// function call
+fetchLaunchData();
