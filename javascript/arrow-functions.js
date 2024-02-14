1ce75cc959b390f7693f314a04417cfc97e02a6b
Aaron Smith <arnhsmith@gmail.com>
2024-02-13 20:35:13 -0500

    updated arrow-functions.js to have one liner concise versions

---
+++ javascript/arrow-functions.js
@@ -0,0 +1,39 @@
+
+const addTwoNumbers = (num1, num2) => {
+    return num1 + num2
+}
+
+console.log(addTwoNumbers(1, 2))
+
+const addTwoNumbersConcise = (num1, num2) => num1 + num2
+console.log(addTwoNumbersConcise(1, 2))
+
+const subtractTwoNumbers = (num1, num2) => {
+    return num1 - num2
+}
+
+console.log(subtractTwoNumbers(3, 2))
+
+const subtractTwoNumbersConcise = (num1, num2) => num1 - num2
+
+console.log(subtractTwoNumbersConcise(3, 2))
+
+const mulitplyTwoNumbers = (num1, num2) => {
+    return num1 * num2
+}
+
+console.log(mulitplyTwoNumbers(2, 8))
+
+const mulitplyTwoNumbersConcise = (num1, num2) => num1 * num2
+
+console.log(mulitplyTwoNumbersConcise(2, 4))
+
+const divideTwoNumbers = (num1, num2) => {
+    return num1 / num2
+}
+
+console.log(divideTwoNumbers(4, 2))
+
+const divideTwoNumbersConcise = (num1, num2) => num1 / num2
+
+console.log(divideTwoNumbersConcise(2, 2))
